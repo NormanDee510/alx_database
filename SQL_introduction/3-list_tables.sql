e6dac49d9a8d5985b7d55ca70515d312c49c1c4e
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-15 16:27:39 -0700

    List tables

---
+++ SQL_introduction/3-list_tables.sql
@@ -0,0 +1,7 @@
+-- List all tables in the specified database
+
+/* Connect to the MySQL server using the provided credentials */
+USE `mysql`;
+
+/* List all tables in the specified database */
+SHOW TABLES;
