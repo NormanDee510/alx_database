46912b52c587aae3cc4ca1afbfb5bb9269b31c9d
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-15 13:13:03 -0700

    List databases

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,5 +1,5 @@
 -- List all databases on the MySQL server
 
 /* Connect to the MySQL server using the provided credentials */
-SELECT SCHEMA_NAME AS DatabaseName
+SELECT SCHEMA_NAME AS NormanDee
 FROM INFORMATION_SCHEMA.SCHEMATA;
