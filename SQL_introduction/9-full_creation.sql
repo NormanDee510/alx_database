d2dfd2e68065c74a0174c92e0caa1831f41ce57e
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-15 17:13:35 -0700

    Full creation

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,18 @@
+-- Create table second_table and insert multiple rows
+
+/* Connect to the specified database using the provided credentials */
+USE `hbtn_0c_0`;
+
+/* Create second_table if it doesn't exist */
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+/* Insert multiple rows into second_table */
+INSERT INTO second_table (id, name, score) VALUES
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8);
