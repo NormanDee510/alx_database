fd6eee1ab8d5be4752e9de085d5d8638ecc23c87
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-15 17:05:10 -0700

    First add

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,8 @@
+-- Insert a new row into the table first_table
+
+/* Connect to the specified database using the provided credentials */
+USE `hbtn_0c_0`;
+
+/* Insert a new row with the specified values into first_table */
+INSERT INTO first_table (id, name)
+VALUES (89, 'Holberton School');
