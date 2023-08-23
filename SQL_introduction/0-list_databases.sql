ae8c7aa976b3eb85d7008ab101b3c7d0fb65627c
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-23 11:28:58 -0700

    List databases

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,4 +1,6 @@
-/* List all databases on the MySQL server */
+-- List all databases on the MySQL server
+
+/* Connect to the MySQL server using the provided credentials */
 SELECT 'Database' AS DatabaseName
 UNION
 SELECT SCHEMA_NAME AS DatabaseName
