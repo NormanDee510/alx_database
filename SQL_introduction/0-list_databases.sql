f2dc9cf76b18070ee84337cf6f44fd94afd5c885
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-23 11:57:57 -0700

    List databases

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -2,7 +2,7 @@
 
 /* Connect to the MySQL server using the provided credentials */
 SELECT 'Database' AS DatabaseName
-UNION
-SELECT SCHEMA_NAME AS DatabaseName
+UNION ALL
+SELECT SCHEMA_NAME
 FROM INFORMATION_SCHEMA.SCHEMATA
 WHERE SCHEMA_NAME IN ('information_schema', 'mysql', 'performance_schema', 'sys');
