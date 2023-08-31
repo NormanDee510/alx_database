5c83436495cadaa6b6911ec4a0d1ff4deeb18a45
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-31 14:54:56 -0700

    List databases

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,9 +1,28 @@
 -- List all databases on the MySQL server
 
 /* Connect to the MySQL server using the provided credentials */
-SELECT 'Database' AS DatabaseName
-UNION ALL
-SELECT SCHEMA_NAME
-FROM INFORMATION_SCHEMA.SCHEMATA
-WHERE SCHEMA_NAME IN ('holbteron_db', 'information_schema', 'my_db_01', 'my_db_02', 'my_db_03', 'mysql', 'performance_schema', 'sys')
-ORDER BY DatabaseName ASC;
+import subprocess
+
+mysql_command = "mysql -hlocalhost -uroot -p < 0-list_databases.sql"
+password = "your_mysql_root_password"
+
+# Using subprocess to execute the command and capture the output
+try:
+    completed_process = subprocess.run(
+        mysql_command,
+        input=password.encode(),
+        shell=True,
+        text=True,
+        capture_output=True
+    )
+    
+    if completed_process.returncode == 0:
+        output = completed_process.stdout.strip()
+        databases = output.split('\n')[1:]
+        print("Databases:")
+        for db in databases:
+            print(db)
+    else:
+        print("Error executing the command.")
+except subprocess.CalledProcessError:
+    print("Error executing the command.")
