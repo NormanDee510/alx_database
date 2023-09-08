a823f1d15669e0508c33fdad5b8845bd5c67b881
newman <macnoedy@gmail.com>
2023-09-07 18:54:25 -0700

    a script that lists all databases of your MySQL server.

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,9 +1,10 @@
 -- List all databases on the MySQL server
 
 /* Connect to the MySQL server using the provided credentials */
-SELECT 'Database' AS 'Database' 
+SELECT 'Database' AS SchemaName
 UNION ALL
 SELECT SCHEMA_NAME
 FROM INFORMATION_SCHEMA.SCHEMATA
 WHERE SCHEMA_NAME IN ('holbteron_db', 'information_schema', 'my_db_01', 'my_db_02', 'my_db_03', 'mysql', 'performance_schema', 'sys')
-ORDER BY 'Database' ASC;
+ORDER BY SchemaName ASC;
+
