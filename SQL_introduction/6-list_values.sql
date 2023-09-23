450e4ba4e7347a142764aafda8bfad59985ee3f7
newman <macnoedy@gmail.com>
2023-09-22 21:33:07 -0700

    list values

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,8 +1,3 @@
 -- List all rows of the table first_table
 
-/* Connect to the specified database using the provided credentials */
-USE `hbtn_0c_0`;
-
-/* Query to retrieve all rows from the first_table */
-SELECT *
-FROM first_table;
+SELECT * FROM first_table;
