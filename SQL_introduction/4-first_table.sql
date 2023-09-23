4b292dc070ee18f84c53bc36c06342b664953a0c
newman <macnoedy@gmail.com>
2023-09-22 21:24:02 -0700

    first table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,11 +1,6 @@
 -- Create a table called first_table in the current database
 
-/* Connect to the specified database using the provided credentials */
-USE `hbtn_0c_0`;
-
-/* Create the first_table if it does not exist */
-CREATE TABLE IF NOT EXISTS `first_table` (
-  `id` INT NOT NULL,
-  `name` VARCHAR(256),
-  PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS first_table(
+    id INT,
+    name VARCHAR(256)
 );
