3aa7fd8b37c79c3904a856f20fdc47b81d2de1db
newman <macnoedy@gmail.com>
2023-09-22 22:53:41 -0700

    never empty

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -2,7 +2,7 @@
 USE your_database_name; -- Replace your_database_name with the actual database name
 
 -- Create the id_not_null table if it doesn't exist
-CREATE TABLE IF NOT EXISTS id_not_null (
+CREATE TABLE IF NOT EXISTS id_not_null(
     id INT DEFAULT 1,
-    name VARCHAR(256)
+    name VARCHAR(256) NOT NULL
 );
