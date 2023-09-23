ef21c0c3f84cf94764436fabb79e943fdecf6296
newman <macnoedy@gmail.com>
2023-09-22 21:28:05 -0700

    full table

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,9 +1,3 @@
 -- Print the full description of the table first_table
 
-/* Connect to the specified database using the provided credentials */
-USE `hbtn_0c_0`;
-
-/* Query to get the full description of the first_table */
-SELECT TABLE_NAME, CREATE_TABLE
-FROM information_schema.TABLES
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE first_table;
