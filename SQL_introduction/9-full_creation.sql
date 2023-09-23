919a03b98bfb8cbaf97ae5adb451b67ce7ac7b61
newman <macnoedy@gmail.com>
2023-09-22 21:43:36 -0700

    full creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,9 +1,5 @@
 -- Create table second_table and insert multiple rows
 
-/* Connect to the specified database using the provided credentials */
-USE `hbtn_0c_0`;
-
-/* Create second_table if it doesn't exist */
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
