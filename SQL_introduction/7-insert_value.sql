bbae3b0f5b16371b276b410b548c70205ddfdc91
newman <macnoedy@gmail.com>
2023-09-22 21:36:49 -0700

    insert value

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,8 +1,3 @@
 -- Insert a new row into the table first_table
 
-/* Connect to the specified database using the provided credentials */
-USE `hbtn_0c_0`;
-
-/* Insert a new row with the specified values into first_table */
-INSERT INTO first_table (id, name)
-VALUES (89, 'Holberton School');
+INSERT INTO first_table(id,name) values(89,'Holberton School'); SELECT * FROM hbtn_0c_0.first_table WHERE id = 89;
