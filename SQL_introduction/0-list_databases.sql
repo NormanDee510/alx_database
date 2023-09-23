0d95cfeb9cb5090e479738a034caafd0767a3644
newman <macnoedy@gmail.com>
2023-09-22 21:15:06 -0700

    list all databases

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,11 +1,5 @@
 -- List all databases on the MySQL server
 
-/* Connect to the MySQL server using the provided credentials */
-SELECT 'Database'
-UNION ALL
-SELECT SCHEMA_NAME
-FROM INFORMATION_SCHEMA.SCHEMATA
-WHERE SCHEMA_NAME IN ('information_schema', 'mysql', 'performance_schema', 'sys')
+SHOW databases;
 ORDER BY SCHEMA_NAME ASC;
-
 
