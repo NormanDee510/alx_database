f1d2189b86be3e02c1575248ac265967615e5754
newman <macnoedy@gmail.com>
2023-09-22 21:21:10 -0700

    list all tables

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,7 +1,2 @@
 -- List all tables in the specified database
-
-/* Connect to the MySQL server using the provided credentials */
-USE `mysql`;
-
-/* List all tables in the specified database */
-SHOW TABLES;
+SHOW tables;
