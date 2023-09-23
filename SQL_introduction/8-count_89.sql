bec1e8c51a28da8f958b97b893ec9b7f5af910f9
newman <macnoedy@gmail.com>
2023-09-22 21:40:18 -0700

    count 89

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,7 +1,3 @@
 -- Display the number of records with id = 89 in the table first_table
 
-/* Connect to the specified database using the provided credentials */
-USE `hbtn_0c_0`;
-
-/* Count the number of records with id = 89 in first_table */
 SELECT COUNT(*) FROM first_table WHERE id = 89;
